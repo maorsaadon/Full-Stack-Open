9f85f952dccdf5f91ef7ef3935cdb5b6e31034db
maorsaadon <maor199724@gmail.com>
2024-07-29 13:42:18 +0300

    update part 1 a

--- part1/part1/src/App.jsx
+++ part1/part1/src/App.jsx
@@ -1,30 +1,57 @@
 
-const Hello = (props) => {
-  console.log(props)
+const Header = (props) => {
   return (
-    <div>
-      <p>Hellow world {props.name}, you are {props.age} years old</p>
+    <div> 
+      <h1>{props.course}</h1>
     </div>
   )
 }
 
-const Footer = () => {
+const Content = (props) => {
   return(
     <div>
-      greeting app created by <a href='https://github.com/maorsaadon'>Maor Florentin</a>
+      <Part name = {props.part[0]} exerciseNum = {props.exercise[0]}/>
+      <Part name = {props.part[1]} exerciseNum = {props.exercise[1]}/>
+      <Part name = {props.part[2]} exerciseNum = {props.exercise[2]}/>
+    </div>
+    
+  )
+}
+
+const Part = (props) => {
+  return(
+    <div>
+      <p>{props.name} {props.exerciseNum}</p> 
+    </div>
+    
+  )
+}
+
+const Total = (props) => {
+  return(
+    <div>
+      <p>Number of exercise {props.sum}</p>
     </div>
   )
 }
 
 
 const App = () => {
-  const friends = ["Maor", "Aviya", "Ron"]
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+  const sum = exercises1+exercises2+exercises3
+
   return (
-    <>
-      <h1>Greeting</h1>
-      <p>{friends}</p>
-      <Footer />
-    </>
+    <div>
+      <Header course={course} />
+      <Content part = {[part1, part2, part3]} exercise = {[exercises1, exercises1, exercises1]}/>
+      <Total sum = {sum}/>
+    </div>
   )
 }
 
